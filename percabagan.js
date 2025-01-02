8b52dbf52e8bd9c07c28489cafed75a12d288378
rikityt <141697865+rikityt@users.noreply.github.com>
2025-01-02 16:03:53 +0700

    Add files via upload

---
+++ percabagan.js
@@ -0,0 +1,9 @@
+let angka = 12;
+
+if (angka > 10) {
+    console.log("Angka lebih besar dari 10");
+} else if (angka == 10) {
+    console.log("Angka sama dengan 10");
+} else {
+    console.log("Angka kurang dari 10");
+}
